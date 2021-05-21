a37632f8559f203d33b2aa6e39394bf50a04e3da
Mike <mclancy96@gmail.com>
2021-05-21 19:47:14 -0400

    Create Current component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Current from './components/Current.js';
 
 function App() {
   return (
     <div className="App">
+      <Current/>
     </div>
   );
 }
