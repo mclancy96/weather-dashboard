bcaa7d16aab749a7b0a031b6ea37ef943eaa44aa
Mike <mclancy96@gmail.com>
2021-06-01 00:24:20 -0400

    Finish dashboard

--- src/components/LocationChange.js
+++ src/components/LocationChange.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+require('dotenv').config();
+
 let alertValue = null;
 
 class LocationChange extends Component{
@@ -16,7 +18,7 @@ class LocationChange extends Component{
         } else {
             countryState = this.state.country;
         }
-        fetch(process.env.SERVER_URL+"dashboard", {
+        fetch(process.env.SERVER_URL +"dashboard", {
           method: 'post',
           headers: {'Content-Type':'application/json'},
           body: JSON.stringify({
