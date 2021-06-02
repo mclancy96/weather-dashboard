392b23cf32160306333bb917cdd0a6b57e06a4c5
Mike <mclancy96@gmail.com>
2021-06-02 13:20:27 -0400

    Fix alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,47 +3,49 @@ import React, {Component} from "react";
 class Alert extends Component{
     render(){
         const {alerts} = this.props.weatherData; 
-        let eventName = alerts.map((alt) => {return<td key={Math.random()+Math.random()}><strong>{alt.event}</strong></td>})
-        let office = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}>{alt.sender_name}</td>})            
-        let starts = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}>{alt.start}</td>})            
-        let ends = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}>{alt.end}</td>})            
-        let details = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}><p>{alt.description}</p></td>})            
-        return (
-            <div>
-                <h4 className='text-center'>Weather Alerts</h4> 
-                {alerts===null?<p className="text-center">No active weather alerts</p>:
-                    <div className="table-responsive">
-                        <table className="text-center table table-dark table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Alert</th>
-                                    {eventName}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th>Issuing Office</th>
-                                    {office}
-                                </tr>
-                                <tr>
-                                    <th>Starts</th>
-                                    {starts}
-                                </tr>
-                                <tr>
-                                    <th>Ends</th>
-                                    {ends}
-                                </tr>
-                                <tr>
-                                    <th>Details</th>
-                                    {details}
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div> 
-                }     
-            </div>                   
-
-        )
+        if(alerts && alerts[0].event){
+            let eventName = alerts.map((alt) => {return<td key={Math.random()+Math.random()}><strong>{alt.event}</strong></td>})
+            let office = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}>{alt.sender_name}</td>})            
+            let starts = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}>{alt.start}</td>})            
+            let ends = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}>{alt.end}</td>})            
+            let details = alerts.map((alt)=>{return<td key={Math.random()+Math.random()}><p>{alt.description}</p></td>})            
+            return (
+                <div>
+                    <h4 className='text-center'>Weather Alerts</h4> 
+                    {alerts===null?<p className="text-center">No active weather alerts</p>:
+                        <div className="table-responsive">
+                            <table className="text-center table table-dark table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Alert</th>
+                                        {eventName}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>Issuing Office</th>
+                                        {office}
+                                    </tr>
+                                    <tr>
+                                        <th>Starts</th>
+                                        {starts}
+                                    </tr>
+                                    <tr>
+                                        <th>Ends</th>
+                                        {ends}
+                                    </tr>
+                                    <tr>
+                                        <th>Details</th>
+                                        {details}
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div> 
+                    }     
+                </div>                   
+            )
+        }
+        
     }
     
 }
